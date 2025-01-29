bcdaa0c387cc4e82a7709a0229005547ec4df614
guillaume-mathe <guillaume.mathe@gmail.com>
2025-01-29 14:04:59 +0100

    Sélecteur mes espaces: évolutions (ajout HR) + doc en ligne

--- src/webcomponents/menu/index.js
+++
@@ -1,5 +0,0 @@
-import '@material/web/button/filled-button.js';
-import '@material/web/button/outlined-button.js';
-import '@material/web/checkbox/checkbox.js';
-import '@material/web/menu/menu';
-import '@material/web/menu/menu-item';
--- src/webcomponents/menu/select-mes-espaces.js
+++ src/webcomponents/menu/select-mes-espaces.js
@@ -32,7 +32,18 @@ class SelectMesEspaces extends LitElement {
       position: relative;
     }
     span {
-      position: relative;
+        position: relative;
+        width: 100%;
+    }
+    hr {
+        border-color: var(--md-sys-color-outline-variant);
+        border-width: 0 0 thin;
+        border-style: solid;
+    }
+    #selecteur-demarches-icon {
+        width: 1em; 
+        height: 1em; 
+        font-size: 1.5rem;
     }
   `;
 
@@ -52,7 +63,7 @@ class SelectMesEspaces extends LitElement {
 
     render() {
         return html`
-            <span style="position: relative; width: 100%">
+            <span>
         <md-outlined-button
                 id="select-mes-espaces-button"
                 has-icon
@@ -60,7 +71,7 @@ class SelectMesEspaces extends LitElement {
                 @click=${this.toggleMenu}
         >
           Mes autres démarches
-          <svg slot="icon" viewBox="0 0 125.92 88.51" style="width: 1em; height: 1em; font-size: 1.5rem;">
+          <svg slot="icon" viewBox="0 0 125.92 88.51" id="selecteur-demarches-icon"">
                 <path d="M68.36 7.12v.02a44.25 44.25 0 100 74.22l57.56-37.38z" fill="var(--md-sys-color-primary)"/>
                 <path
                 d="M32.36 31.86c4.58-4.58 12.2-6.52 19.41-1.39L30.5 51.76c-5.4-8.88-1.53-16.5 1.87-19.9m28.61 16.86a14.88 14.88 0 01-4.27 7.48c-6.59 6.59-15.6 4.78-18.52 2.7l29.89-29.88a30.17 30.17 0 00-4.09-5.2c-9.7-9.71-27.95-12.55-40.14-.35-11.86 11.86-10.27 30.23.62 41.11a28.6 28.6 0 0040.7.07 27.56 27.56 0 007.75-15.93z"
@@ -75,11 +86,12 @@ class SelectMesEspaces extends LitElement {
                 @close-menu=${this.onCloseMenu}
         >
                     ${this.links.map(
-                            (link) => html`
+                            (link) => 
+                                    typeof link === 'string' ? html`<hr/>`
+                                    : html`
                                 <md-menu-item data-href=${link.href}>
                                     <div slot="headline">${link.label}</div>
-                                </md-menu-item>
-                            `
+                                </md-menu-item>`
                     )}
         </md-menu>
       </span>
@@ -89,6 +101,7 @@ class SelectMesEspaces extends LitElement {
     toggleMenu() {
         const menuEl = this.shadowRoot.getElementById('select-mes-espaces-menu');
         const anchorEl = this.shadowRoot.getElementById('select-mes-espaces-button');
+        menuEl.anchorElement = anchorEl;
 
         if (!menuEl.open) {
             menuEl.style.minWidth = `${anchorEl.offsetWidth}px`;
