0a2c7159881147297db4f37f712fd77502982854
Benrouijal Imade (DIN) <imade.benrouijal@etat.ge.ch>
2025-07-23 14:34:08 +0000

    Ajout du max width pour les headers

--- packages/webcomponents/ge-header-public/src/ge-header-public.js
+++ packages/webcomponents/ge-header-public/src/ge-header-public.js
@@ -1,76 +1,84 @@
-import { html, css, LitElement } from "lit";
-import { customElement } from "lit/decorators.js";
+import {css, html, LitElement} from "lit";
+import {customElement, property} from "lit/decorators.js";
 
 
 export @customElement("ge-header-public")
 class GeHeaderPublic extends LitElement {
-  static styles = css`
-    :host {
-      display: block;
-      width: 100%;
-      font-family: Arial, sans-serif;
-    }
 
-    .header {
-      display: flex;
-      padding: 16px 32px;
-      width: 100%;
-      justify-content: space-between;
-      align-items: center;
-      flex: 1 0 0;
-      border-bottom: 1px solid var(--md-sys-color-outline-variant, #d4d2cf);
-      background: var(--md-sys-color-surface-surface, #fff);
-    }
+    @property({type: String})
+    maxWidth = css`unset`;
 
-    a.logo-section {
-      text-decoration: none;
-      color: inherit;
-    }
 
-    .logo-section {
-      display: flex;
-      align-items: center;
-      gap: 16px;
-      cursor: pointer;
-    }
+    static styles = css`
+        :host {
+            display: block;
+            font-family: Arial, sans-serif;
+        }
 
-    .title {
-      font-size: 2rem;
-      font-weight: 500;
-      color: var(--md-sys-color-on-surface-variant, #333);
-    }
+        header {
+            border-bottom: 1px solid var(--md-sys-color-outline-variant, #d4d2cf);
+            background: var(--md-sys-color-surface-surface, #fff);
+        }
 
-    .account-actions {
-      display: flex;
-      align-items: center;
-    }
+        .header {
+            display: flex;
+            padding: 16px 32px;
+            justify-content: space-between;
+            align-items: center;
+            flex: 1 0 0;
+        }
 
-    .manage-account {
-      background: none;
-      border: none;
-      color: var(--md-sys-color-primary, #6200ee);
-      font-weight: bold;
-      cursor: pointer;
-      font-size: 14px;
-      text-decoration: underline;
-      padding: 8px;
-    }
-  `;
+        a.logo-section {
+            text-decoration: none;
+            color: inherit;
+        }
 
-  render() {
-    return html`
-      <header class="header" role="banner">
-        <a
-         href="https://www.ge.ch/" class="logo-section" target="gech">
-          <img
-            src="https://static.app.ge.ch/theme/icons/common/header/header-armoiries-light.svg"
-            alt="République et canton de Genève"
-            width="27"
-            height="45"
-          />
-          <span class="title">ge.ch</span>
-        </a>
-      </header>
+        .logo-section {
+            display: flex;
+            align-items: center;
+            gap: 16px;
+            cursor: pointer;
+        }
+
+        .title {
+            font-size: 2rem;
+            font-weight: 500;
+            color: var(--md-sys-color-on-surface-variant, #333);
+        }
+
+        .account-actions {
+            display: flex;
+            align-items: center;
+        }
+
+        .manage-account {
+            background: none;
+            border: none;
+            color: var(--md-sys-color-primary, #6200ee);
+            font-weight: bold;
+            cursor: pointer;
+            font-size: 14px;
+            text-decoration: underline;
+            padding: 8px;
+        }
     `;
-  }
+
+    render() {
+        return html`
+            <header role="banner">
+                <div class="header" style="max-width: ${this.maxWidth}">
+                    <a
+                            href="https://www.ge.ch/" class="logo-section" target="gech">
+                        <img
+                                src="https://static.app.ge.ch/theme/icons/common/header/header-armoiries-light.svg"
+                                alt="République et canton de Genève"
+                                width="27"
+                                height="45"
+                        />
+                        <span class="title">ge.ch</span>
+                    </a>
+                </div>
+            </header>
+        `;
+    }
 }
